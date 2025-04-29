a1b68747d2d8078c7ca90951583f30bc9624ae98
Heiko Hardt <heiko@hardt.dev>
2025-04-30 01:09:25 +0200

    [TASK] Fix namespace issue in TYPO3Context

--- Tests/Acceptance/Context/TYPO3Context.php
+++ Tests/Acceptance/Context/TYPO3Context.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 namespace HeikoHardt\DevTemplate\Tests\Acceptance\Context;
 
 use Behat\Behat\Context\Context;
-use HeikoHardt\E2environment\Behat\Context\Typo3Context as BaseTypo3Context;
+use HeikoHardt\Behat\TYPO3Extension\Context\Typo3Context as BaseTypo3Context;
 
 class TYPO3Context extends BaseTypo3Context implements Context {}
